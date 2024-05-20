daf2c91c0225433bd66208a7268043b81ac915cb
juan-mkd <juanestebancalderonovalle@gmail.com>
2024-05-20 10:30:15 -0500

    se acomodaron las files

--- YourNomiNow 2.9 Arreglo certificados y frond registro/desprendible/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DesprendibleConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'desprendible'
--- YourNomiNow 2.9 Arreglo certificados y frond registro/empleado/static/empleado/js/certific.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("btnImprimir").addEventListener("click", function() {
-    var doc = new jsPDF();
-    doc.html(document.getElementById("contenido"), {
-      callback: function(pdf) {
-        pdf.save("constancia.pdf");
-      }
-    });
-  });
