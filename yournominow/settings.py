1662b55377e7510fb6c7a15f4176c0e6ee65c99e
Juan <127887246+Juan-mkd@users.noreply.github.com>
2024-06-16 13:34:03 -0500

    Update settings.py

--- yournominow/settings.py
+++ yournominow/settings.py
@@ -28,11 +28,7 @@ DEBUG = True
 
 # ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'yourdomain.com']
 
-ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-    '[::1]',
-]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
